d93f666f542f7c2901085c721499d1db5a102624
Amanfromearth <anurag3b@gmail.com>
2024-11-06 10:39:28 +0530

    adding auth callback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-// import { auth } from "@/auth";
 import Catalog from "@/components/Home/catalog";
 import NewLaunched from "@/components/Home/NewLaunched";
 import Subscriptions from "@/components/Home/Subscriptions";
 import React from "react";
 
 const page = async () => {
-  // const session = await auth();
   return (
     <main className="bg-[#E7E7E3] flex flex-col">
       <div className="flex flex-col w-full min-h-screen px-12">
